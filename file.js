fc14caa7822a2ad4e0bf91c111f25b4e3be59c93
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-09-04 11:26:53 +0300

    1

---
+++ file.js
@@ -0,0 +1,11 @@
+// const giveMinute = 110;
+// const hours = Math.floor(giveMinute % 60);
+// const minute = giveMinute % 60;
+// const modifiedHours = String(hours).padStart(2, 0);
+// const modifiedMinutes = String(minute).padStart(2, 0);
+// console.log(`${modifiedHours}:${modifiedMinutes}`);
+
+//3. Напишите цыкл, который выводит в консоль
+//числа от max до min по убыванию
+const max = 50;
+const min = 23;
